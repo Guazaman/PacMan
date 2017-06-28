22c157ee740e62505872e33bdfb643afac4ab294
Miguel Contreras Corona <miguel050395@hotmail.com>
2017-06-28 15:37:55 -0500

    Video 7: Consume casi todas las bolitas

--- PacMan/Assets/Scripts/Tile.cs
+++ PacMan/Assets/Scripts/Tile.cs
@@ -6,6 +6,10 @@ public class Tile : MonoBehaviour {
 
 	public bool isPortal;
 
+	public bool isPellet;
+	public bool isSuperPellet;
+	public bool didConsume;
+
 	public GameObject portalReceiver;
 
 }
